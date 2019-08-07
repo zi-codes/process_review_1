f6c584faedde31e65658bc6a65e77a516695b208
Student <ziwei.polaris@gmail.com>
2019-08-07 14:33:20 +0100

    collect marks method working

--- lib/markbook.rb
+++ lib/markbook.rb
@@ -1,11 +1,23 @@
 class Markbook
 
+  def collect_marks(string)
+    answer = []
+    green = collect_colour("Green",string)
+    answer << green
+    amber = collect_colour("Amber",string)
+    answer << amber
+    red = collect_colour("Red",string)
+    answer << red
+    answer.join(", ")
+
+  end
+
   def collect_colour(colour,string)
 
     array = string.split(",")
     number = array.count(colour)
 
-    "#{colour} : #{number}"
+    "#{colour}: #{number}"
 
   end
 
--- spec/markbook_spec.rb
+++ spec/markbook_spec.rb
@@ -2,15 +2,19 @@ require 'markbook'
 
 describe Markbook do
     it "can count how many red marks are achieved" do
-      expect(subject.collect_colour("Red","Red,Red,Red")).to eq "Red : 3"
+      expect(subject.collect_colour("Red","Red,Red,Red")).to eq "Red: 3"
     end
 
     it "can count how many green marks are achieved" do
-      expect(subject.collect_colour("Green","Red,Red,Red,Green,Amber,Green")).to eq "Green : 2"
+      expect(subject.collect_colour("Green","Red,Red,Red,Green,Amber,Green")).to eq "Green: 2"
     end
 
     it "can count how many amber marks are achieved" do
-      expect(subject.collect_colour("Amber","Amber,Red,Red,Red,Green,Amber,Amber,Amber,Green")).to eq "Amber : 4"
+      expect(subject.collect_colour("Amber","Amber,Red,Red,Red,Green,Amber,Amber,Amber,Green")).to eq "Amber: 4"
+    end
+
+    it "can collect marks for each colour and return a string with the results" do
+      expect(subject.collect_marks("Amber,Red,Red,Red,Green,Amber,Amber,Amber,Green")).to eq "Green: 2, Amber: 4, Red: 3"
     end
 
 end
